7a9d2d5d16d44f0593a12a29c1a3b7a49351a9a8
Sriram Govindan <sriramgovindanwork@gmail.com>
2021-11-25 20:15:46 -0800

    added restful api to access models

---
+++ app.py
@@ -0,0 +1,118 @@
+from flask import Flask, request, send_file
+from flask_cors import CORS
+from Models.Transformer import TransformerNetwork
+from torchvision import transforms
+import flask
+from PIL import Image
+import torch
+import os
+import io
+import PIL
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+"""
+MODELS
+"""
+device_name = "cuda" if torch.cuda.is_available() else "cpu"
+device = torch.device(device=device_name)
+
+model_dict = {}
+for i in os.listdir("./SavedModels"):
+    path = os.path.join(".", "SavedModels", i)
+
+    checkpoint = torch.load(path)
+    model = TransformerNetwork().to(device)
+    model.load_state_dict(checkpoint["model_state_dict"])
+
+    model_name = i.split(".model")[0]
+    model_dict.update({model_name: model})
+
+
+"""
+Feeds image into model returning the filtered image
+
+image_bytes: The image in bytes
+
+filter_model: the model we are using to add the filter to the image
+
+size: The size of the image in pixels
+
+returns the PIL image object
+"""
+
+
+def transform_image(image_bytes: bytes,
+                    filter_model: torch.nn.Module,
+                    size: int) -> PIL.Image.Image:
+
+    transformer_list = [transforms.Resize(size),
+                        transforms.CenterCrop(size),
+                        transforms.ToTensor(),
+                        transforms.Lambda(lambda x: x.mul(255))]
+
+    pic_tf = transforms.Compose(transformer_list)
+    img = Image.open(io.BytesIO(image_bytes))
+
+    with torch.no_grad():
+        filter_model.eval()
+
+        img_tensor = torch.unsqueeze(pic_tf(img), dim=0).to(device)
+        output = torch.squeeze(filter_model(img_tensor), dim=0).cpu()
+
+        output = output.detach().clone().numpy()
+
+        filtered = output.transpose(1, 2, 0).clip(0, 255).astype("uint8")
+        filtered = Image.fromarray(filtered)
+
+        return filtered
+
+
+"""
+Applies a filter to a specified image
+
+file_path: where the image file is located must be of the type
+.png, .jpg, or .jpeg. A string
+
+filter_name: the name of the filter we want to apply to the image.
+Options are cudi, edtaonisl, mosaic, scream, starrynight, and muse. A string
+
+size: the size of the new filtered image, the size must be less than or equal too
+the actual size of the image. Options are micro, small, medium, large, or mega. A string
+
+returns the image in bytes
+"""
+
+
+@app.route('/apply-filter', methods=['GET'])
+def apply_filter():
+    file = request.files['file']
+    name = request.form['name']
+    size = request.form['size']
+
+    img_bytes = file.read()
+
+    size_dict = {"micro": 500,
+                 "small": 750,
+                 "medium": 1000,
+                 "large": 1500,
+                 "mega": 2250}
+
+    if size.lower() not in list(size_dict.keys()):
+        flask.abort(500)
+
+    image = transform_image(img_bytes,
+                            model_dict[name],
+                            size_dict[size])
+
+    img_io = io.BytesIO()
+    image.save(img_io, "JPEG", quality=70)
+    img_io.seek(0)
+
+    return send_file(img_io, mimetype='image/jpeg')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
---
+++ utilities.py
@@ -0,0 +1,101 @@
+import torch
+import numpy as np
+from PIL import Image
+from torchvision import transforms
+
+from Models.Transformer import TransformerNetwork
+
+torch.manual_seed(108)
+
+
+def get_transformer_list(normalize: bool,
+                         range_255: bool) -> list:
+    transformation_list = [transforms.Resize(256),
+                           transforms.CenterCrop(256),
+                           transforms.ToTensor()]
+    if range_255:
+        transformation_list.append(transforms.Lambda(lambda x: x.mul(255)))
+        if normalize:
+            transformation_list.append(transforms.Normalize(mean=np.array([123.675, 116.28, 103.53]),
+                                                            std=np.array([1, 1, 1])))
+    else:
+        if normalize:
+            transformation_list.append(transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                            std=[0.229, 0.224, 0.225]))
+    return transformation_list
+
+
+def transform_image(state_dict_path: str,
+                    content_image_path,
+                    save_path=None,
+                    save=False,
+                    normalize=False,
+                    range_255=False):
+    device_name = "cuda" if torch.cuda.is_available() else "cpu"
+    # device_name = "cpu"
+    device = torch.device(device=device_name)
+
+    checkpoint = torch.load(state_dict_path)
+    model = TransformerNetwork().to(device)
+    model.load_state_dict(checkpoint["model_state_dict"])
+
+    with torch.no_grad():
+        model.eval()
+
+        pic_tf_list = get_transformer_list(normalize=normalize,
+                                           range_255=range_255)[2:]
+
+        pic_tf = transforms.Compose(pic_tf_list)
+
+        content_image = load_image(content_image_path)
+        content_tensor = torch.unsqueeze(pic_tf(content_image), dim=0).to(device)
+
+        display_image(torch.squeeze(model(content_tensor), dim=0).cpu(),
+                      range_255=range_255,
+                      normalize=normalize,
+                      save=save,
+                      path=save_path)
+
+
+def compute_gram(image: torch.Tensor):
+    b, c, h, w = image.size()
+    image = image.view(b, c, h * w)
+    t_image = torch.transpose(image, 1, 2)
+    gram_matrix = torch.bmm(image, t_image) / (c * h * w)
+    return gram_matrix
+
+
+def display_image(data,
+                  save=False,
+                  path=None,
+                  normalize=False,
+                  range_255=False):
+    img = data.detach().clone().numpy()
+
+    if range_255:
+        if normalize:
+            mean = np.array([123.675, 116.28, 103.53])
+            std = np.array([1, 1, 1])
+            img = (img * std + mean).transpose(1, 2, 0).clip(0, 255).astype("uint8")
+        else:
+            img = img.transpose(1, 2, 0).clip(0, 255).astype("uint8")
+
+    else:
+        if normalize:
+            std = np.array([0.229, 0.224, 0.225]).reshape((3, 1, 1))
+            mean = np.array([0.485, 0.456, 0.406]).reshape((3, 1, 1))
+            img = ((img * std + mean).transpose(1, 2, 0) * 255.0).clip(0, 255).astype("uint8")
+
+        else:
+            img = (img.transpose(1, 2, 0) * 255).clip(0, 255).astype("uint8")
+
+    img = Image.fromarray(img)
+    img.show()
+
+    if save:
+        img.save(path)
+
+
+def load_image(filename):
+    img = Image.open(filename).convert('RGB')
+    return img
